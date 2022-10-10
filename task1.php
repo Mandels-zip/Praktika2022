a64d9beebd95fd5aaa13eca5a7c4920d9ea3f2ef
drama0queen <iljagurov@gmail.com>
2022-10-10 14:38:26 +0300

    maintaks

---
+++ task1.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<br>";
+error_reporting(0);
+
+// Задача 1.1
+echo "Меня зовут ". $_POST['fname']. " ".$_POST['sname']. "<br>Мне " .$_POST['age'] . "<br> Я писал на языках <br>";
+    
+foreach($_POST['checklist'] as $selected){
+    echo "<li>" .$selected. "</li>";
+}
+
+echo "Мой любимый язык ".$_POST['fav_lang']. "<br>";
+echo $_POST['opinion'];
+     
+ ?>
---
+++ task2.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Вы загрузили файл " .$_FILES['uploadfile']['name']. "<br>Он весит ".$_FILES['uploadfile']['size']. "байт"; 
+?>
