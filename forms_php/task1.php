6213d15bbe044dcef84990e8b9b150ef515cac42
drama0queen <iljagurov@gmail.com>
2022-10-13 00:12:49 +0300

    clearing files

--- forms_php/task1.php
+++ forms_php/task1.php
@@ -1,11 +1,10 @@
 <?php
 
+error_reporting(0);
 echo "<br>";
 
-
-// Задача 1.1
 echo "Меня зовут ". $_POST['fname']. " ".$_POST['sname']. "<br>Мне " .$_POST['age'] . "<br> Я писал на языках <br>";
-    
+
 foreach($_POST['checklist'] as $selected){
     echo "<li>" .$selected. "</li>";
 }
@@ -13,14 +12,14 @@ foreach($_POST['checklist'] as $selected){
 echo "Мой любимый язык ".$_POST['fav_lang']. "<br>";
 echo $_POST['opinion']. "<br>";
 
-
 if (!file_exists($_FILES['uploadfile']['tmp_name']) || !is_uploaded_file($_FILES['uploadfile']['tmp_name']))
 {
     echo 'No upload';
 }
-else{
+else
+{
     echo "Ваш файл был успешно загружен <br>";
     echo "Вы загрузили файл " .$_FILES['uploadfile']['name']. "<br>Он весит ".$_FILES['uploadfile']['size']. "байт"; 
 }
 
- ?>
+?>
